20b973a3cc2a8b4225ff3b1b0ab724f95611785d
Atik2788 <ashanto653.2@gmail.com>
2025-12-16 22:59:10 +0600

    change checkAuth.ts

--- dist/app/middlewares/checkAuth.js
+++ dist/app/middlewares/checkAuth.js
@@ -18,36 +18,75 @@ const jwt_1 = require("../utils/jwt");
 const appError_1 = __importDefault(require("../errorHelpers/appError"));
 const user_model_1 = require("../modules/user/user.model");
 const user_interface_1 = require("../modules/user/user.interface");
-const http_status_1 = __importDefault(require("http-status"));
 const checkAuth = (...authRoles) => (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const accessToken = req.headers.authorization;
-        // console.log(accessToken)
         if (!accessToken || typeof accessToken !== "string") {
             throw new appError_1.default(403, "Access token not found");
         }
-        const verifiToken = (0, jwt_1.verifyToken)(accessToken, env_1.envVars.JWT_ACCESS_SECRET);
-        const isUserExist = yield user_model_1.User.findOne({ email: verifiToken.email });
-        if (!isUserExist) {
-            throw new appError_1.default(http_status_1.default.BAD_REQUEST, "User does not exist");
+        const verifiedToken = (0, jwt_1.verifyToken)(accessToken, env_1.envVars.JWT_ACCESS_SECRET);
+        const user = yield user_model_1.User.findOne({ email: verifiedToken.email });
+        if (!user)
+            throw new appError_1.default(400, "User does not exist");
+        if (user.isActive === user_interface_1.IsActive.BLOCKED || user.isActive === user_interface_1.IsActive.INACTIVE) {
+            throw new appError_1.default(400, `User is ${user.isActive}`);
         }
-        if (isUserExist.isActive === user_interface_1.IsActive.BLOCKED || isUserExist.isActive === user_interface_1.IsActive.INACTIVE) {
-            throw new appError_1.default(http_status_1.default.BAD_REQUEST, `User is ${isUserExist.isActive}`);
-        }
-        if (isUserExist.isDeleted) {
-            throw new appError_1.default(http_status_1.default.BAD_REQUEST, "Usrer is deleted");
-        }
-        if (!verifiToken) {
-            throw new appError_1.default(403, `You are not authorized ${verifiToken}`);
-        }
-        if (!authRoles.includes(verifiToken.role)) {
+        if (user.isDeleted)
+            throw new appError_1.default(400, "User is deleted");
+        // Attach user to req first
+        req.user = verifiedToken;
+        // Case-insensitive role check
+        if (authRoles.length && !authRoles.some(r => r.toUpperCase() === verifiedToken.role.toUpperCase())) {
             throw new appError_1.default(403, "Access Denied for You! You are not authorized");
         }
-        req.user = verifiToken;
         next();
     }
-    catch (error) {
-        next(error);
+    catch (err) {
+        next(err);
     }
 });
 exports.checkAuth = checkAuth;
+/*
+export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Response, next: NextFunction)=>{
+
+    try {
+        const accessToken = req.headers.authorization;
+        // console.log(accessToken)
+        if(!accessToken || typeof accessToken !== "string"){
+            throw new AppError(403, "Access token not found")
+        }
+
+        
+        const verifiToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET as string) as JwtPayload;
+
+        const isUserExist = await User.findOne({email: verifiToken.email})
+
+        if(!isUserExist){
+            throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
+        }
+        if(isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE){
+            throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+        }
+        if(isUserExist.isDeleted){
+            throw new AppError(httpStatus.BAD_REQUEST, "Usrer is deleted")
+        }
+        
+    
+        if(!verifiToken){
+            throw new AppError(403, `You are not authorized ${verifiToken}`)
+        }
+
+        
+        if(!authRoles.includes(verifiToken.role)){
+            throw new AppError(403, "Access Denied for You! You are not authorized")
+        }
+        
+        req.user = verifiToken;
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+*/ 
--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -4,9 +4,48 @@ import { verifyToken } from "../utils/jwt";
 import AppError from "../errorHelpers/appError";
 import { User } from "../modules/user/user.model";
 import { IsActive } from "../modules/user/user.interface";
-import httpStatus from "http-status";
+// import httpStatus from "http-status";
 import { JwtPayload } from "jsonwebtoken";
 
+
+export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const accessToken = req.headers.authorization;
+    if (!accessToken || typeof accessToken !== "string") {
+      throw new AppError(403, "Access token not found");
+    }
+
+    const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET as string) as JwtPayload;
+
+    const user = await User.findOne({ email: verifiedToken.email });
+    if (!user) throw new AppError(400, "User does not exist");
+
+    if (user.isActive === IsActive.BLOCKED || user.isActive === IsActive.INACTIVE) {
+      throw new AppError(400, `User is ${user.isActive}`);
+    }
+
+    if (user.isDeleted) throw new AppError(400, "User is deleted");
+
+    // Attach user to req first
+    req.user = verifiedToken;
+
+    // Case-insensitive role check
+    if (authRoles.length && !authRoles.some(r => r.toUpperCase() === verifiedToken.role.toUpperCase())) {
+      throw new AppError(403, "Access Denied for You! You are not authorized");
+    }
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+
+
+
+
+/*
 export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Response, next: NextFunction)=>{
 
     try {
@@ -48,3 +87,5 @@ export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Re
         next(error)
     }
 }
+
+*/
