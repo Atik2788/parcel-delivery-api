8cd8dcfe27610bbb1ac014d6eb131dc49b41fc0e
Atik2788 <ashanto653.2@gmail.com>
2025-12-20 23:54:26 +0600

    change frontend url

--- src/app.ts
+++ src/app.ts
@@ -19,25 +19,15 @@ import "./app/config/passport";
     //   saveUninitialized: false
     // }))
 
-    const allowedOrigins = [
-        "http://localhost:3000",
-        process.env.FRONTEND_URL as string,
-        ];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://parcel-delivery-service-frontend.netlify.app",
+    ];
 
-    app.use(
-    cors({
-        origin: (origin, callback) => {
-        if (!origin) return callback(null, true); // Postman / server-to-server
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        return callback(new Error("CORS not allowed"));
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-    );
+    app.use(cors({
+        origin: allowedOrigins,
+        credentials: true
+    }));
 
 
     app.use(express.json());
